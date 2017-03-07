4e1e35756564df4df0a2f61e00e793a923e8d4f3
Connor McElroy <cmcelroy@cougar.cs.ksu.edu>
2017-03-07 14:18:13 -0600

    buggy

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var sqlite3 = require('sqlite3');
+var db = new sqlite3.Database('scrumtatic.sqlite3');
+
+var migrate = require('./src/migrations');
+migrate(db);\\db.run("INSERT INTO projects (name) values ('title')");
+console.log(db.run("SELECT * FROM projects"));
