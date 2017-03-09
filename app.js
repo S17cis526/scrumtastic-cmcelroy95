9bb17cfe0d0bbe5a704eb7ae01ba54ccb4188cbb
Connor McElroy <cmcelroy@WIN2.CS.KSU.EDU>
2017-03-09 14:17:10 -0600

    What happened today.

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 "use strict";
 
-var sqlite3 = require('sqlite3');
-var db = new sqlite3.Database('scrumtatic.sqlite3');
 
-var migrate = require('./src/migrations');
-migrate(db);\\db.run("INSERT INTO projects (name) values ('title')");
-console.log(db.run("SELECT * FROM projects"));
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('scrumtastic.sqlite3', function(err) {
+  if(err) console.error(err);
+});
+
+var migrate = require('./lib/migrate');
+migrate(db, 'migrations', function(err){
+  db.serialize(function(){
+    db.run("INSERT INTO projects (name) values (?)", ['title']);
+    db.all("SELECT * FROM projects", [], function(err, rows){
+      if(err) return console.error(err);
+      console.log("ROWS:", rows);
+    });
+  });
+});
